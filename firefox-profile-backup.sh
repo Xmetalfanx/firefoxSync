d00136198a0b39a762ea9bd9b4545fff2a7a02de
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-02-10 17:24:00 -0500

    major refactoring and code cleanup

--- firefox-profile-backup.sh
+++ firefox-profile-backup.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
 clear
-
+# include config files 
 . "configs/common.cfg"
 . "configs/actions.cfg"
 
-#############################################################################
-
-# 1. Check for Firefox Profile dir.... if it doesn't exist there is nothing to backup
-# should return 1 (inverse check being used so 1 means the dir DOES exit)
-    # idea ... since it didn't return "true" (0) it doesn't get to the post && part
-[ -d "${ffProfileFolder}" ] && echo "Proeeding to backup INTIAL firefox profiles" && userPrompt || echo "Firefox profile location does not exit, nothing to do, Exiting script" || exit 
-
-# 2. Check if the destination folder exists, if it doesn't CREATE it
-checkDirExistsAndCreate "${ffProfileBackupFolder}"
-userPrompt
-
-# 3 - Create backup of user's INTIAL profiles .. this should never BE overwritten if the backup location exists and has existing data 
-backupIntialProfiles && userPrompt
-
-
-backupFFProfiles && userPrompt
+metaBackupFFProfiles
